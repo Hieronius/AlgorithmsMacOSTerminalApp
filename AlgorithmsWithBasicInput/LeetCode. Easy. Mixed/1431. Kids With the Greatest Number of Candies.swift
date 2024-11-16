085c8691d9176d230824d2576ee802010e0faf1b
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-16 03:48:00 +0300

    Solve 1431 LeetCode problem

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/1431. Kids With the Greatest Number of Candies.swift
@@ -0,0 +1,29 @@
+//
+//  1431. Kids With the Greatest Number of Candies.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 15.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
+
+		 var result: [Bool] = []
+
+		 guard let max = candies.max() else { return [false] }
+
+		 for kid in candies {
+			 if (kid + extraCandies) >= max {
+				 result.append(true)
+			 } else {
+				 result.append(false)
+			 }
+		 }
+
+		 return result
+ }
+ }
+ */
