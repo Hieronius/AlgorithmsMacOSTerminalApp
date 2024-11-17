557970dd85f0ea0b2aa0414110e4af64259c2bda
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-17 03:42:37 +0300

    solve 1470 leetcode problem

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/1470. Shuffle the Array.swift
@@ -0,0 +1,29 @@
+//
+//  1470. Shuffle the Array.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+	 let midIndex = nums.count / 2
+
+	 let leftArray = Array(nums[0..<midIndex])
+	 let rightArray = Array(nums[midIndex..<nums.count])
+
+	 var result: [Int] = []
+
+	 for index in 0..<midIndex {
+		 result.append(leftArray[index])
+		 result.append(rightArray[index])
+	 }
+
+	 return result
+
+	 }
+ }
+ */
