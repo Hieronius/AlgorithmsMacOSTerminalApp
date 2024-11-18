3d3345ca3acfdc9eebcf294b91b1668929baca48
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-18 03:44:46 +0300

    solve LeetCode problem N 2235

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2235. Add Two Integers.swift
@@ -0,0 +1,16 @@
+//
+//  2235. Add Two Integers.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 12.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func sum(_ num1: Int, _ num2: Int) -> Int {
+		 num1 + num2
+	 }
+ }
+ */
