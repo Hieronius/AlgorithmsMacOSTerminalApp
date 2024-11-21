ddef1262f726585ef4206ad2beb9ae0af3dc5c3a
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-21 04:34:40 +0300

    solve leet code N 2469

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2469. Convert the Temperature.swift
@@ -0,0 +1,17 @@
+//
+//  2469. Convert the Temperature.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func convertTemperature(_ celsius: Double) -> [Double] {
+
+		 return [celsius + 273.15, celsius * 1.80 + 32.00]
+	 }
+ }
+ */
