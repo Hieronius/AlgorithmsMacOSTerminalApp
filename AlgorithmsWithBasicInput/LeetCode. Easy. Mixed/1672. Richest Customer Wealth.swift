9846e4b563d3d7ed2c563bcd3600ea1c42b7639a
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-24 04:09:32 +0300

    Solve problem 1672 leetcode

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/1672. Richest Customer Wealth.swift
@@ -0,0 +1,27 @@
+//
+//  1672. Richest Customer Wealth.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ // Speed O(n)
+ // Memory O(1)
+ func maximumWealth(_ accounts: [[Int]]) -> Int {
+
+	 var maxWealth = 0
+	 var currentWealth = 0
+
+	 for client in accounts {
+		 for fund in client {
+			 currentWealth += fund
+		 }
+		 maxWealth = max(maxWealth, currentWealth)
+		 currentWealth = 0
+	 }
+	 return maxWealth
+ }
+ */
