96faa04c3f03605fade702380a3cb094ae227972
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-26 03:39:16 +0300

    solve leetcode N 2924

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2942. Find Words Containing Character.swift
@@ -0,0 +1,24 @@
+//
+//  2942. Find Words Containing Character.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func findWordsContaining(_ words: [String], _ x: Character) -> [Int] {
+
+ var arrayOfIndices: [Int] = []
+
+ for (index, word) in words.enumerated() {
+	 if word.contains(x) {
+		 arrayOfIndices.append(index)
+	 }
+ }
+ return arrayOfIndices
+ }
+ }
+ */
