67de40761b17a6c3b257e986a6074d308abda012
Arsenty Halimovsky <ghost12@yandex.ru>
2024-11-28 03:49:22 +0300

    Solve leetcode N 88

---
+++ AlgorithmsWithBasicInput/150 most popular questions/Array/88. Merge Sorted Array.swift
@@ -0,0 +1,19 @@
+//
+//  88. Merge Sorted Array.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 17.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+ func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+ var newAr = Array(nums1[0..<m])
+ newAr += nums2
+ nums1 = newAr
+ nums1.sort()
+ }
+ }
+ */
