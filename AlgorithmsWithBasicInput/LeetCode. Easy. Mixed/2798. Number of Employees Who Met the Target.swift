38675975c6d7938f48cedc32cc02fc55328e85f9
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-01 03:46:18 +0300

    Solve LeetCode problem 2798

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2798. Number of Employees Who Met the Target.swift
@@ -0,0 +1,24 @@
+//
+//  2798. Number of Employees Who Met the Target.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 14.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func numberOfEmployeesWhoMetTarget(_ hours: [Int], _ target: Int) -> Int {
+
+	 var result = 0
+
+	 for worker in hours {
+		 if worker >= target {
+			 result += 1
+		 }
+	 }
+	 return result
+ }
+ }
+ */
