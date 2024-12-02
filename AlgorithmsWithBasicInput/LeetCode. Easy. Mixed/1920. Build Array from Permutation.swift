b6decb5f17518c79816b66a4e1840bdafb8d6749
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-02 03:55:32 +0300

    Solve LeetCode N 1920

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/1920. Build Array from Permutation.swift
@@ -0,0 +1,22 @@
+//
+//  1920. Build Array from Permutation.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func buildArray(_ nums: [Int]) -> [Int] {
+		 var result: [Int] = []
+
+		 for num in nums {
+			 result.append(nums[num])
+		 }
+
+	   return result
+	 }
+ }
+ */
