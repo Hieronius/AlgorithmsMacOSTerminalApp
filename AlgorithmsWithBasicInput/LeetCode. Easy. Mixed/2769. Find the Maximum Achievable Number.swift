74ad752e2149c38d914fa90202cd7e9c02d2685b
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-05 03:45:24 +0300

    Solve LeetCode N 2769

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2769. Find the Maximum Achievable Number.swift
@@ -0,0 +1,16 @@
+//
+//  2769. Find the Maximum Achievable Number.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func theMaximumAchievableX(_ num: Int, _ t: Int) -> Int {
+		num + t * 2
+	 }
+ }
+ */
