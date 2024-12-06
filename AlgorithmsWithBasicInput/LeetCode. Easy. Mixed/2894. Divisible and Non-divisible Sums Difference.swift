d0bae89c710bd2acacb8be10a9eea399be504941
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-06 04:11:02 +0300

    Solve LeetCode N 2894

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2894. Divisible and Non-divisible Sums Difference.swift
@@ -0,0 +1,18 @@
+//
+//  2894. Divisible and Non-divisible Sums Difference.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func differenceOfSums(_ n: Int, _ m: Int) -> Int {
+		 let num1 = Array(1...n).filter {$0 % m != 0}.reduce(0, +)
+	 let num2 = Array(1...n).filter {$0 % m == 0}.reduce(0, +)
+	 return num1 - num2
+	 }
+ }
+ */
