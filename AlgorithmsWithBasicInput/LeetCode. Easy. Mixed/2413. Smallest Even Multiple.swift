723b71517f1222176c854ea935661d8e3e2d887a
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-10 03:40:12 +0300

    Solve LeetCode N 2413

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/2413. Smallest Even Multiple.swift
@@ -0,0 +1,25 @@
+//
+//  2413. Smallest Even Multiple.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 14.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func smallestEvenMultiple(_ n: Int) -> Int {
+
+	 guard n % 2 != 0 else { return n }
+
+	 var answer = n
+
+	 while !((answer % 2 == 0) && (answer % n == 0)) {
+		 answer += 1
+	 }
+
+	 return answer
+ }
+ }
+ */
