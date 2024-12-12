11c9123eab4e5e0ed56257f1ef89ed06de312c38
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-12 03:41:56 +0300

    Solve LeetCode N 1603

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/1603. Design Parking System.swift
@@ -0,0 +1,57 @@
+//
+//  1603. Design Parking System.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ Speed O(1)
+ Memory O(n)
+class ParkingSystem {
+
+	var big: Int
+	var medium: Int
+	var small: Int
+
+	init(_ big: Int, _ medium: Int, _ small: Int) {
+		self.big = big
+		self.medium = medium
+		self.small = small
+	}
+
+	func addCar(_ carType: Int) -> Bool {
+
+		switch carType {
+
+		case 1:
+			if self.big > 0 {
+				self.big -= 1
+				return true
+			} else {
+				return false
+			}
+
+		case 2:
+			if self.medium > 0 {
+				self.medium -= 1
+				return true
+			} else {
+				return false
+			}
+
+		case 3:
+			if self.small > 0 {
+				self.small -= 1
+				return true
+			} else {
+				return false
+			}
+
+		default:
+			return false
+		}
+	}
+ */
