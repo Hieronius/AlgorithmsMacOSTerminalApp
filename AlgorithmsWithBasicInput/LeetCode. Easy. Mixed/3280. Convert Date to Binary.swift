2f081a0b736fd549b981737cb5c648d948084654
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-14 03:41:23 +0300

    Solve LeetCode 3280

---
+++ AlgorithmsWithBasicInput/LeetCode. Easy. Mixed/3280. Convert Date to Binary.swift
@@ -0,0 +1,19 @@
+//
+//  3280. Convert Date to Binary.swift
+//  AlgorithmsWithBasicInput
+//
+//  Created by Арсентий Халимовский on 13.11.2024.
+//
+
+import Foundation
+
+/*
+ class Solution {
+	 func convertDateToBinary(_ date: String) -> String {
+		 date.split(separator: "-")
+		 .map { Int($0) ?? -1 }
+		 .map { String($0, radix: 2) }
+		 .joined(separator: "-")
+	 }
+ }
+ */
