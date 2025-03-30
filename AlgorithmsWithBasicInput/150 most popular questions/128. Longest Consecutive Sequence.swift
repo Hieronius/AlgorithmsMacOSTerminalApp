35c0c32fc9ae30e189485fc1842859329d618200
Arsenty Halimovsky <ghost12@yandex.ru>
2025-03-30 06:13:58 +0300

    Solve LeetCode № 128

--- AlgorithmsWithBasicInput/150 most popular questions/128. Longest Consecutive Sequence.swift
+++ AlgorithmsWithBasicInput/150 most popular questions/128. Longest Consecutive Sequence.swift
@@ -7,21 +7,60 @@
 
 import Foundation
 
-let nums1 = [0,1,2,4,5,7] // answer ["0->2","4->5","7"]
-let nums2 = [0,2,3,4,6,8,9] // answer ["0","2->4","6","8->9"]
+//let nums1 = [0,1,2,4,5,7] // answer ["0->2","4->5","7"]
+//let nums2 = [0,2,3,4,6,8,9] // answer ["0","2->4","6","8->9"]
+//
+//class Solution {
+//
+//	func summaryRanges(_ nums: [Int]) -> [String] {
+//
+//		guard !nums.isEmpty else { return "" }
+//		guard nums.count > 1 else { return "\(nums[0])" }
+//
+//		var result: [String] = []
+//		var leftIndex = 0
+//		var rightIndex = 1
+//		var window = ""
+//
+//		// nums.count = 5, last element = rightIndex == 4
+//		print("hello")
+//		while rightIndex < nums.count {
+//			print("hello")
+//
+//			if nums[leftIndex] + 1 == nums[rightIndex] && !window.contains("->") {
+//
+//				window.append("\(nums[leftIndex])->")
+//				leftIndex += 1
+//				rightIndex += 1
+//
+//			} else if nums[leftIndex] + 1 != nums[rightIndex] && !window.contains("->") {
+//
+//				result.append("\(nums[leftIndex])")
+//				leftIndex += 1
+//				rightIndex += 1
+//
+//			} else if nums[leftIndex] + 1 == nums[rightIndex] && window.contains("->") {
+//
+//				leftIndex += 1
+//				rightIndex += 1
+//
+//
+//			} else if nums[leftIndex] + 1 != nums[rightIndex] && window.contains("->") {
+//
+//				window.append("\(nums[rightIndex])")
+//				result.append(window)
+//				window = ""
+//				leftIndex += 1
+//				rightIndex += 1
+//
+//			}
+//		}
+//		result.append(window)
+//		return result
+//	}
+//}
 
-class Solution {
-
-	func summaryRanges(_ nums: [Int]) -> [String] {
-
-		guard !nums.isEmpty else { return "" }
-		guard nums.count > 1 else { return "\(nums[0])" }
-
-		var result: [String] = []
-		var leftIndex = 0
-
-		while leftIndex < nums.count {
-
-			
-	}
-}
+//let solution = Solution()
+print("hello")
+//print(solution.summaryRanges(nums1))
+//print(solution.summaryRanges(nums2))
